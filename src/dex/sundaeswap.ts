bcf96800d720ee7acf922fcf2b5c234e5b4e5b31
Sluder <zachary.sluder@gmail.com>
2023-06-29 11:49:32 -0400

    Fix Sundaeswap fee

--- src/dex/sundaeswap.ts
+++ src/dex/sundaeswap.ts
@@ -111,7 +111,7 @@ export class SundaeSwap extends BaseDex {
                 ? BigInt(parameters.TotalLpTokens)
                 : 0n;
             liquidityPool.poolFeePercent = typeof parameters.LpFeeNumerator === 'number' && typeof parameters.LpFeeDenominator === 'number'
-                ? parameters.LpFeeNumerator / parameters.LpFeeDenominator
+                ? (parameters.LpFeeNumerator / parameters.LpFeeDenominator) * 100
                 : 0;
         } catch (e) {
             return liquidityPool;
