22db3111a540f935c798ce5b3e9469e8b5994017
Sluder <zachary.sluder@gmail.com>
2023-12-22 14:29:03 -0500

    Fix teddy fee

--- src/dex/api/teddyswap-api.ts
+++ src/dex/api/teddyswap-api.ts
@@ -51,7 +51,7 @@ export class TeddyswapApi extends BaseApi {
                 const [poolNftPolicyId, poolNftName] = poolResponse.id.split('.');
                 liquidityPool.poolNft = new Asset(poolNftPolicyId, Buffer.from(poolNftName, 'utf8').toString('hex'));
                 liquidityPool.lpToken = new Asset(poolResponse.lockedLQ.asset.currencySymbol, Buffer.from(poolResponse.lockedLQ.asset.tokenName, 'utf8').toString('hex'));
-                liquidityPool.poolFeePercent = (1 - (poolResponse.poolFeeNum / poolResponse.poolFeeDenum)) * 10;
+                liquidityPool.poolFeePercent = (1 - (poolResponse.poolFeeNum / poolResponse.poolFeeDenum)) * 100;
                 liquidityPool.identifier = liquidityPool.lpToken.identifier();
 
                 return liquidityPool;
