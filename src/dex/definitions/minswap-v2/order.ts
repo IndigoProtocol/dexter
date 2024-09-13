6d27f494d0710715e5767fa02b34c244d007c830
unknown <zachary.sluder@gmail.com>
2024-09-13 14:09:48 -0400

    Fix Minswapv2 datum

--- src/dex/definitions/minswap-v2/order.ts
+++ src/dex/definitions/minswap-v2/order.ts
@@ -70,7 +70,7 @@ export default {
                   constructor: 0,
                   fields: [
                     {
-                      bytes: DatumParameterKey.SenderPubKeyHash,
+                      bytes: DatumParameterKey.SenderStakingKeyHash,
                     }
                   ]
                 }
